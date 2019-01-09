f71a18fbf4ca9c304181e28064f267f168f57bc1
Christian Westgaard <cwe@enonic.com>
2019-01-09 14:19:07 +0100

    Less fieldsets

--- src/main/resources/assets/react/Collection.jsx
+++ src/main/resources/assets/react/Collection.jsx
@@ -77,169 +77,157 @@ export const Collection = ({
 				const InsertButton = ({index, insert, value}) => <button type="button" onClick={() => insert(index+1, value)}>+</button>;
 
 				return (<Form>
-					<Fieldset legend="Collection">
+					<LabeledField label='Name' name="name" />
 
-						<LabeledField label='Name' name="name" />
+					<Fieldset legend="Request">
+						<Fieldset legend="Url">
+							<FieldArray
+								name="urls"
+								render={({insert, swap, remove}) => (
+									<div>
+										{urls && urls.map((anUrl, index) => {
+											/*console.log(JSON.stringify({
+												anUrl,
+												index
+											}, null, 4));*/
+											return (<div key={`urls[${index}]`}>
+												{/*<button type="button" onClick={() => insert(index, '')}>⎀</button>*/}
+												<Field name={`urls[${index}]`} />
+												{urls.length > 1 ? <RemoveButton index={index} remove={remove}/> : null}
+												{index ? <MoveUpButton index={index} swap={swap}/> : null}
+												{index < urls.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
+												<InsertButton index={index} insert={insert} value={''}/>
+											</div>);
+										})}
+									</div>
+								)}
+							/>
+						</Fieldset>
 
-						<Fieldset legend="Request">
-							<Fieldset legend="Url">
+						{pathRange
+							? (<Fieldset legend="Path range">
+								<LabeledField label="Min" name="pathRange.min"/>
+								<Label label="Max">
+									<Field name="pathRange.max"/>
+								</Label>
+								<SetFieldValueButton field='pathRange' value={null} text="Remove path range"/>
+							</Fieldset>)
+							: <SetFieldValueButton field='pathRange' value={{min: 0, max: 1}} text="Add path range"/>
+						}
+
+						{queryRange
+							? (<Fieldset legend="Query range">
+								<LabeledField label="Name" name="queryRange.name"/>
+								<LabeledField label="Min" name="queryRange.min" type="number"/>
+								<LabeledField label="Max" name="queryRange.max" type="number"/>
+								<SetFieldValueButton field='queryRange' value={null} text="Remove query range"/>
+							</Fieldset>)
+							: <SetFieldValueButton field='queryRange' value={{name: '', min: 0, max: 1}} text="Add query range"/>
+						}
+
+						{headers && headers.length
+							? (<Fieldset legend="Headers">
 								<FieldArray
-									name="urls"
+									name="headers"
 									render={({insert, swap, remove}) => (
-										<div>
-											{urls && urls.map((anUrl, index) => {
-												/*console.log(JSON.stringify({
-													anUrl,
-													index
-												}, null, 4));*/
-												return (<div key={`urls[${index}]`}>
-													{/*<button type="button" onClick={() => insert(index, '')}>⎀</button>*/}
-													<Field name={`urls[${index}]`} />
-													{urls.length > 1 ? <RemoveButton index={index} remove={remove}/> : null}
-													{index ? <MoveUpButton index={index} swap={swap}/> : null}
-													{index < urls.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
-													<InsertButton index={index} insert={insert} value={''}/>
-												</div>);
+										<Table headers={['Name', 'Value', 'Action(s)']}>
+											{headers && headers.length > 0 && headers.map(({name, value}, index) => {
+												console.log(JSON.stringify({
+													index,
+													name,
+													value
+												}, null, 4));
+												return (<tr key={`headers[${index}]`}>
+													{/*<button type="button" onClick={() => insert(index, {name: '', value: ''})}>+</button>*/}
+													<td><Field name={`headers[${index}].name`}/></td>
+													<td><Field name={`headers[${index}].value`}/></td>
+													<td>
+														<RemoveButton index={index} remove={remove}/>
+														{index ? <MoveUpButton index={index} swap={swap}/> : null}
+														{index < headers.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
+														<InsertButton index={index} insert={insert} value={{name: '', value: ''}}/>
+													</td>
+												</tr>);
 											})}
-										</div>
+										</Table>
 									)}
 								/>
-							</Fieldset>
+							</Fieldset>)
+							: <SetFieldValueButton field='headers' value={[{name: '', value: ''}]} text="Add header(s)"/>
+						}
 
-							{pathRange
-								? (<Fieldset legend="Path range">
-									<LabeledField label="Min" name="pathRange.min"/>
-									<Label label="Max">
-										<Field name="pathRange.max"/>
-									</Label>
-									<SetFieldValueButton field='pathRange' value={null} text="Remove path range"/>
-								</Fieldset>)
-								: <SetFieldValueButton field='pathRange' value={{min: 0, max: 1}} text="Add path range"/>
-							}
+						<LabeledField label="Delay" name="delay"/>
 
-							{queryRange
-								? (<Fieldset legend="Query range">
-									<LabeledField label="Name" name="queryRange.name"/>
-									<LabeledField label="Min" name="queryRange.min" type="number"/>
-									<LabeledField label="Max" name="queryRange.max" type="number"/>
-									<SetFieldValueButton field='queryRange' value={null} text="Remove query range"/>
-								</Fieldset>)
-								: <SetFieldValueButton field='queryRange' value={{name: '', min: 0, max: 1}} text="Add query range"/>
-							}
+					</Fieldset>{/* Request */}
 
-							{headers && headers.length
-								? (<Fieldset legend="Headers">
-									<FieldArray
-										name="headers"
-										render={({insert, swap, remove}) => (
-											<Table headers={['Name', 'Value', 'Action(s)']}>
-												{headers && headers.length > 0 && headers.map(({name, value}, index) => {
-													console.log(JSON.stringify({
-														index,
-														name,
-														value
-													}, null, 4));
-													return (<tr key={`headers[${index}]`}>
-														{/*<button type="button" onClick={() => insert(index, {name: '', value: ''})}>+</button>*/}
-														<td><Field name={`headers[${index}].name`}/></td>
-														<td><Field name={`headers[${index}].value`}/></td>
-														<td>
-															<RemoveButton index={index} remove={remove}/>
-															{index ? <MoveUpButton index={index} swap={swap}/> : null}
-															{index < headers.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
-															<InsertButton index={index} insert={insert} value={{name: '', value: ''}}/>
-														</td>
-													</tr>);
-												})}
-											</Table>
-										)}
-									/>
-								</Fieldset>)
-								: <SetFieldValueButton field='headers' value={[{name: '', value: ''}]} text="Add header(s)"/>
-							}
+					{node && node.scrape && node.scrape.length
+						? (<FieldArray
+								name="node.scrape"
+								render={({insert, swap, remove}) => (
+									<div>
+										{node.scrape && node.scrape.length > 0 && node.scrape.map(({field, dataExpr}, index) => (
+											<Fieldset key={index} legend={`Scrape[${index}]`}>
+												{/*<button type="button" onClick={() => insert(index, {field: '', dataExpr: ''})}>+</button>*/}
+												<LabeledField label="Field" name={`node.scrape[${index}].field`}/>
+												<LabeledField label="Data extraction expression" name={`node.scrape[${index}].dataExpr`}/>
+												<RemoveButton index={index} remove={remove}/>
+												{index ? <MoveUpButton index={index} swap={swap}/> : null}
+												{index < node.scrape.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
+												<InsertButton index={index} insert={insert} value={{field: '', dataExpr: ''}}/>
+											</Fieldset>
+										))}
+									</div>
+								)}
+							/>)
+						: <SetFieldValueButton field='node.scrape' value={[{field: '', dataExpr: ''}]} text="Add scrape field"/>
+					}
 
-							<LabeledField label="Delay" name="delay"/>
 
-						</Fieldset>{/* Request */}
 
-						<Fieldset legend="Node">
-
-							{node && node.scrape && node.scrape.length
-								? (<Fieldset legend="Scrape">
-									<FieldArray
-										name="node.scrape"
-										render={({insert, swap, remove}) => (
-											<div>
-												{node.scrape && node.scrape.length > 0 && node.scrape.map(({field, dataExpr}, index) => (
-													<Fieldset key={index} legend={`Scrape[${index}]`}>
-														{/*<button type="button" onClick={() => insert(index, {field: '', dataExpr: ''})}>+</button>*/}
-														<LabeledField label="Field" name={`node.scrape[${index}].field`}/>
-														<LabeledField label="Data extraction expression" name={`node.scrape[${index}].dataExpr`}/>
-														<RemoveButton index={index} remove={remove}/>
-														{index ? <MoveUpButton index={index} swap={swap}/> : null}
-														{index < node.scrape.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
-														<InsertButton index={index} insert={insert} value={{field: '', dataExpr: ''}}/>
-													</Fieldset>
-												))}
+					{node && node.download && node.download.length
+						? (<FieldArray
+								name="node.download"
+								render={({insert, swap, remove}) => (
+									<div>
+										{node.download && node.download.length > 0 && node.download.map((aDownloadExpression, index) => (
+											<div key={index}>
+												{/*<button type="button" onClick={() => insert(index, '')}>⎀</button>*/}
+												<Field name={`node.download[${index}]`} />
+												<RemoveButton index={index} remove={remove}/>
+												{index ? <MoveUpButton index={index} swap={swap}/> : null}
+												{index < node.download.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
+												<InsertButton index={index} insert={insert} value={''}/>
 											</div>
-										)}
-									/>
-								</Fieldset>)
-								: <SetFieldValueButton field='node.scrape' value={[{field: '', dataExpr: ''}]} text="Add scrape field"/>
-							}
+										))}
+									</div>
+								)}
+							/>)
+						: <SetFieldValueButton field='node.download' value={['']} text="Add download expression(s)"/>
+					}
 
+					{node && node.crawl && node.crawl.length
+						? (<FieldArray
+								name="node.crawl"
+								render={({insert, swap, remove}) => (
+									<div>
+										{node.crawl && node.crawl.length > 0 && node.crawl.map(({dynamic, urlExpr}, index) => (
+											<Fieldset key={index} legend={`Crawl[${index}]`}>
+												{/*<button type="button" onClick={() => insert(index, {dynamic: false, urlExpr: ''})}>+</button>*/}
+												<LabeledField label="Dynamic" name={`node.crawl[${index}].dynamic`} type="checkbox"/>
+												<LabeledField label="Url extraction expression" name={`node.crawl[${index}].urlExpr`}/>
+												<RemoveButton index={index} remove={remove}/>
+												{index ? <MoveUpButton index={index} swap={swap}/> : null}
+												{index < node.crawl.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
+												<InsertButton index={index} insert={insert} value={{dynamic: false, urlExpr: ''}}/>
+											</Fieldset>
+										))}
+									</div>
+								)}
+							/>)
+						: <SetFieldValueButton field='node.crawl' value={[{dynamic: false, urlExpr: ''}]} text="Add crawl expression(s)"/>
+					}
 
-
-							{node && node.download && node.download.length
-								? (<Fieldset legend="Download">
-									<FieldArray
-										name="node.download"
-										render={({insert, swap, remove}) => (
-											<div>
-												{node.download && node.download.length > 0 && node.download.map((aDownloadExpression, index) => (
-													<div key={index}>
-														{/*<button type="button" onClick={() => insert(index, '')}>⎀</button>*/}
-														<Field name={`node.download[${index}]`} />
-														<RemoveButton index={index} remove={remove}/>
-														{index ? <MoveUpButton index={index} swap={swap}/> : null}
-														{index < node.download.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
-														<InsertButton index={index} insert={insert} value={''}/>
-													</div>
-												))}
-											</div>
-										)}
-									/>
-								</Fieldset>)
-								: <SetFieldValueButton field='node.download' value={['']} text="Add download expression(s)"/>
-							}
-
-							{node && node.crawl && node.crawl.length
-								? (<Fieldset legend="Crawl">
-									<FieldArray
-										name="node.crawl"
-										render={({insert, swap, remove}) => (
-											<div>
-												{node.crawl && node.crawl.length > 0 && node.crawl.map(({dynamic, urlExpr}, index) => (
-													<Fieldset key={index} legend={`Crawl[${index}]`}>
-														{/*<button type="button" onClick={() => insert(index, {dynamic: false, urlExpr: ''})}>+</button>*/}
-														<LabeledField label="Dynamic" name={`node.crawl[${index}].dynamic`} type="checkbox"/>
-														<LabeledField label="Url extraction expression" name={`node.crawl[${index}].urlExpr`}/>
-														<RemoveButton index={index} remove={remove}/>
-														{index ? <MoveUpButton index={index} swap={swap}/> : null}
-														{index < node.crawl.length-1 ? <MoveDownButton index={index} swap={swap}/> : null}
-														<InsertButton index={index} insert={insert} value={{dynamic: false, urlExpr: ''}}/>
-													</Fieldset>
-												))}
-											</div>
-										)}
-									/>
-								</Fieldset>)
-								: <SetFieldValueButton field='node.crawl' value={[{dynamic: false, urlExpr: ''}]} text="Add crawl expression(s)"/>
-							}
-
-						</Fieldset>{/* Node */}
-						<SubmitButton text="Save collection"/>
-					</Fieldset>{/* Collection */}
+					<SubmitButton text="Save collection"/>
 				</Form>);
 			}}
 		/>
