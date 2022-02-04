b05b364393780b9b2e1a3e5ca7b9ce3fa4e13406
Christian Westgaard <cwe@enonic.com>
2022-02-04 10:24:03 +0100

    ValidationError must be available as it is already in use by app-explorer

---
+++ src/main/typeScript/lib/explorer/collection/types.d.ts
@@ -0,0 +1,14 @@
+import type {RequiredNodeProperties} from '../types.d';
+
+
+export interface CollectionNode extends RequiredNodeProperties {
+	//collector,
+	//createdTime,
+	//creator,
+	//documentCount,
+	documentTypeId :string
+	//interfaces,// = [],
+	language :string
+	//modifiedTime,
+	//modifier
+}
