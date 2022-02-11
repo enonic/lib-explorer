741bee28509fe269b8ce86f94c3e84792727d3a7
Christian Westgaard <cwe@enonic.com>
2022-02-11 10:43:36 +0100

    Export all the constants

--- src/main/resources/lib/explorer/index.ts
+++ src/main/resources/lib/explorer/index.ts
@@ -1,7 +1,7 @@
-//export * from '/lib/explorer/constants'; // ReferenceError: "APP_EXPLORER" is not defined
-export {
+export * from '/lib/explorer/constants'; // ReferenceError: "APP_EXPLORER" is not defined
+/*export {
 	APP_EXPLORER
-} from '/lib/explorer/constants';
+} from '/lib/explorer/constants';*/
 
 //export {list as getApplications} from '/lib/explorer/application';
 export {create, update} from '/lib/explorer/document';
