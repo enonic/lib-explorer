a254009ef4380f1542b2f70b8a85d56acf9e5618
Christian Westgaard <comlock@gmail.com>
2022-07-25 15:07:13 +0200

    getThesaurus and allowedLanguages

---
+++ src/main/resources/lib/explorer/thesaurus/getThesaurus.ts
@@ -0,0 +1,48 @@
+import type {RepoConnection} from '/lib/explorer/types/index.d';
+
+
+import {
+	FOLDER_THESAURI,
+	NT_THESAURUS
+} from '/lib/explorer/constants';
+import {coerceThesaurus} from '/lib/explorer/thesaurus/coerceThesaurus';
+
+
+export function getThesaurus({
+	// Required
+	connection, // Connecting many places leeds to loss of control over principals, so pass a connection around.
+	// Optional
+	_id: idParam,
+	_name: nameParam
+} :{
+	// Required
+	connection :RepoConnection
+	// Optional
+	_id ?:string
+	_name ?:string
+}) {
+	const key = idParam || (nameParam ? `/${FOLDER_THESAURI}/${nameParam}` : null);
+	if (!key) {
+		throw new Error('getThesaurus(): Missing required named parameter _id or _name!');
+	}
+
+	const node = connection.get(key);
+	if (!node) {
+		const msg = `Unable to get thesaurus with _id:${idParam} or _name:${nameParam}`;
+		log.error(msg);
+		throw new Error(msg);
+	}
+
+	const {
+		_id,
+		_path,
+		_nodeType
+	} = node;
+	if (_nodeType !== NT_THESAURUS) {
+		const msg = `Node with id:${_id} and _path:${_path} is not a thesaurus! rather _nodeType:${_nodeType}`;
+		log.error(msg);
+		throw new Error(msg);
+	}
+
+	return coerceThesaurus(node);
+}
