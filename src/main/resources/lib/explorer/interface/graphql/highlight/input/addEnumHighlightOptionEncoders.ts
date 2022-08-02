5f85204c5100065a77208fcb9d5b980832d4d4e5
Christian Westgaard <comlock@gmail.com>
2022-08-02 12:29:59 +0200

    Use constants

--- src/main/resources/lib/explorer/interface/graphql/highlight/input/addEnumHighlightOptionEncoders.ts
+++ src/main/resources/lib/explorer/interface/graphql/highlight/input/addEnumHighlightOptionEncoders.ts
@@ -1,15 +1,13 @@
 import type {Glue} from '../../utils/Glue';
 
 
+import {HIGHLIGHT_OPTION_ENCODERS} from '@enonic/js-utils';
 import {GQL_ENUM_HIGHLIGHT_OPTION_ENCODERS} from './constants';
 
 
 export function addEnumHighlightOptionEncoders({glue} :{glue :Glue}) {
 	return glue.addEnumType({
 		name: GQL_ENUM_HIGHLIGHT_OPTION_ENCODERS,
-		values: [
-			'default',
-			'html'
-		]
+		values: HIGHLIGHT_OPTION_ENCODERS
 	});
 }
