a8031bf91d4560bd5da85ab57a00c3c7f9b55702
Christian Westgaard <comlock@gmail.com>
2022-08-05 10:35:44 +0200

    Type task.progress.infoObj for GraphQL #190

--- src/main/resources/lib/explorer/types/Task.d.ts
+++ src/main/resources/lib/explorer/types/Task.d.ts
@@ -1,21 +1,23 @@
 import type {Application} from '../../../index.d';
+import type {AnyObject} from './Utility';
 
 
 export type TaskName = string;
 
-export interface Task {
+export interface Task<InfoObj = AnyObject> {
 	description :string
-    id :string
-    name :string // TaskName
-    state :string
-    application :string // ApplicationKey
-    user :string
-    startTime :string
-    progress: {
-      info :string
-      current :number
-      total :number
-    }
+	id :string
+	name :string // TaskName
+	state :string
+	application :string // ApplicationKey
+	user :string
+	startTime :string
+	progress: {
+		info :string // might be json :)
+		infoObj ?:InfoObj // Used in GraphQL as GraphQLJSON
+		current :number
+		total :number
+	}
 }
 
 export type TaskDescriptor = `${Application.Key}:${TaskName}`;
