efffebda4909470041fb7504dcd77f139c1864ba
Christian Westgaard <comlock@gmail.com>
2023-01-18 09:57:08 +0100

    Add HTTP_HEADERS.EXPLORER_INTERFACE_NAME to request headers #235

--- src/main/resources/lib/explorer/types/Request.d.ts
+++ src/main/resources/lib/explorer/types/Request.d.ts
@@ -1,19 +1,24 @@
 import type {
 	AnyObject,
+	EmptyObject,
 	StringObject
 } from './Utility';
 
 
+import {HTTP_HEADERS} from '@enonic/explorer-utils';
+
+
 export type Headers = {
-	Accept ?:string
-	'Accept-Charset' ?:string
-	'Accept-Encoding' ?:string
-	Authorization ?:string
-	'Explorer-Log-Query' ?:'1'
-	'Explorer-Log-Synonyms-Query' ?:'1'
-	'Explorer-Log-Synonyms-Query-Result' ?:'1'
-	'If-None-Match' ?:string
-	'User-Agent' ?:string
+	Accept?: string
+	'Accept-Charset'?: string
+	'Accept-Encoding'?: string
+	Authorization?: string
+	[HTTP_HEADERS.EXPLORER_INTERFACE_NAME]?: string
+	'Explorer-Log-Query'?: '1'
+	'Explorer-Log-Synonyms-Query'?: '1'
+	'Explorer-Log-Synonyms-Query-Result'?: '1'
+	'If-None-Match'?: string
+	'User-Agent'?: string
 }
 
 export type Method = 'GET'|'POST'|'HEAD'|'PUT'|'DELETE'|'PATCH'
@@ -22,44 +27,44 @@ export type Mode = 'edit'|'inline'|'live'|'preview'
 
 
 export type HttpClientRequest = {
-	auth ?:{
-		user :string
-		password :string
+	auth?: {
+		user: string
+		password: string
 	}
-	body ?:string|AnyObject // (string | object) Body content to send with the request, usually for POST or PUT requests. It can be of type string or stream.
-	certificates ?:unknown
-	clientCertificate ?:unknown
-	connectionTimeout ?:number
-	contentType ?:string
-	followRedirects ?:boolean
-	headers ?:Headers
-	method ?:Method
-	multipart ?:Array<AnyObject>
-	params ?:StringObject
-	proxy ?:{
-		host :string
-		port :number
-		user :string
-		password :string
+	body?: string|AnyObject // (string | object) Body content to send with the request, usually for POST or PUT requests. It can be of type string or stream.
+	certificates?: unknown
+	clientCertificate?: unknown
+	connectionTimeout?: number
+	contentType?: string
+	followRedirects?: boolean
+	headers?: Headers
+	method?: Method
+	multipart?: Array<AnyObject>
+	params?: StringObject
+	proxy?: {
+		host: string
+		port: number
+		user: string
+		password: string
 	}
-	queryParams ?:StringObject
-	readTimeout ?:number
-	url :string
+	queryParams?: StringObject
+	readTimeout?: number
+	url: string
 }
 
 
 export type EnonicXpRequest<
-	Params extends StringObject = {},
-	PathParams extends StringObject = {}
+	Params extends StringObject = EmptyObject,
+	PathParams extends StringObject = EmptyObject
 > = {
-	body ?:string
-	contextPath ?:string
-	headers ?:Headers
-	host ?:string
-	method ?:Method
-	mode ?:Mode
-	params ?:Params
-	path ?:string
-	pathParams ?:PathParams
-	rawPath ?:string
+	body?: string
+	contextPath?: string
+	headers?: Headers
+	host?: string
+	method?: Method
+	mode?: Mode
+	params?: Params
+	path?: string
+	pathParams?: PathParams
+	rawPath?: string
 }
