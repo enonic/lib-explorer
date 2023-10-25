bcc271d10029e3ea2cc1fda9b9a968da67f8b6cc
Christian Westgaard <comlock@gmail.com>
2023-10-25 10:22:29 +0200

    Fix #287 Scheduled tasks fail because task config.name isn't properly removed

--- src/main/resources/lib/explorer/scheduler/createOrModifyJob.ts
+++ src/main/resources/lib/explorer/scheduler/createOrModifyJob.ts
@@ -1,9 +1,12 @@
+import type {CollectorTaskConfig} from '/lib/explorer/types/index.d';
+import type {CreateScheduledJobParams} from '/lib/xp/scheduler';
+
 import {toStr} from '@enonic/js-utils';
 import {detailedDiff} from 'deep-object-diff';
 import deepEqual from 'fast-deep-equal';
 
-//import HumanDiff from 'human-object-diff';
-//import * as HumanDiff from 'human-object-diff'; // This fails when doing app-explorer development build
+// import HumanDiff from 'human-object-diff';
+// import * as HumanDiff from 'human-object-diff'; // This fails when doing app-explorer development build
 
 import {
 	USER_EXPLORER_APP_NAME,
@@ -13,13 +16,12 @@ import {
 	create as createJob,
 	get as getJob,
 	modify as modifyJob
-	//@ts-ignore
 } from '/lib/xp/scheduler';
 
 
-/*const { diff: diffJob } = new HumanDiff({
-objectName: 'job'
-});*/
+// const { diff: diffJob } = new HumanDiff({
+// 	objectName: 'job'
+// });
 
 const USER = `user:${USER_EXPLORER_APP_ID_PROVIDER}:${USER_EXPLORER_APP_NAME}`;
 
@@ -31,6 +33,13 @@ export function createOrModifyJob({
 	name,
 	schedule,
 	user = USER
+}: {
+	config: CollectorTaskConfig
+	descriptor: string
+	enabled?: boolean
+	name: string
+	schedule: CreateScheduledJobParams<CollectorTaskConfig>['schedule']
+	user?: CreateScheduledJobParams<CollectorTaskConfig>['user']
 }) {
 	const paramsObj = {
 		config,
@@ -51,26 +60,26 @@ export function createOrModifyJob({
 			log.debug(`No changes detected, not updating job with name:${name}`);
 		} else {
 			log.info(`Changes detected in job with name:${name} diff:${toStr(detailedDiff(maybeExisitingJob, paramsObj))}`);
-			//log.info(`Changes detected in job with name:${name} diff:${toStr(diffJob(maybeExisitingJob, paramsObj))}`);
-			const modifiedJob = modifyJob({
+			// log.info(`Changes detected in job with name:${name} diff:${toStr(diffJob(maybeExisitingJob, paramsObj))}`);
+			const modifiedJob = modifyJob<CollectorTaskConfig>({
 				name,
 				editor: (exisitingJob) => {
-					//log.info(`exisitingJob:${toStr(exisitingJob)}`);
+					// log.info(`exisitingJob:${toStr(exisitingJob)}`);
 					exisitingJob.config = config;
 					exisitingJob.descriptor = descriptor;
 					exisitingJob.enabled = enabled;
 					exisitingJob.schedule = schedule;
 					exisitingJob.user = user;
-					//exisitingJob.modifiedTime = new Date(); // Needed?
+					// exisitingJob.modifiedTime = new Date(); // Needed?
 					return exisitingJob;
 				}
 			});
-			//log.info(`modifiedJob:${toStr(modifiedJob)}`);
+			// log.info(`modifiedJob:${toStr(modifiedJob)}`);
 			return modifiedJob;
 		}
 	} else { // Job doesn't exist
-		const createdJob = createJob(paramsObj);
-		//log.info(`createdJob:${toStr(createdJob)}`);
+		const createdJob = createJob<CollectorTaskConfig>(paramsObj);
+		// log.info(`createdJob:${toStr(createdJob)}`);
 		return createdJob;
 	}
 }
--- src/main/resources/lib/explorer/scheduler/listExplorerJobsThatStartWithName.ts
+++ src/main/resources/lib/explorer/scheduler/listExplorerJobsThatStartWithName.ts
@@ -1,12 +1,11 @@
-//import {toStr} from '@enonic/js-utils';
-
+// import {toStr} from '@enonic/js-utils';
 import {listExplorerJobs} from '/lib/explorer/scheduler/listExplorerJobs';
 
 
-export function listExplorerJobsThatStartWithName({name}) {
+export function listExplorerJobsThatStartWithName({name}: {name: string}) {
 	const explorerJobs = listExplorerJobs();
-	//log.info(`explorerJobs:${toStr(explorerJobs)}`);
+	// log.info(`explorerJobs:${toStr(explorerJobs)}`);
 	const explorerJobsThatStartWithName = explorerJobs.filter(({name: n}) => n.startsWith(name));
-	//log.info(`name:${name} explorerJobsThatStartWithName:${toStr(explorerJobsThatStartWithName)}`);
+	// log.info(`name:${name} explorerJobsThatStartWithName:${toStr(explorerJobsThatStartWithName)}`);
 	return explorerJobsThatStartWithName;
 }
