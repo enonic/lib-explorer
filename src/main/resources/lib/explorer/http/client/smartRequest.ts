796d5f9c68e836eb72bc4172e54b6e08aff6c5d7
Christian Westgaard <comlock@gmail.com>
2023-11-22 10:27:43 +0100

    Fix #289 smartRequest wrongly changes contentType into application/x-www-form-urlencoded

--- src/main/resources/lib/explorer/http/client/smartRequest.ts
+++ src/main/resources/lib/explorer/http/client/smartRequest.ts
@@ -1,10 +1,11 @@
 import type {HttpClient} from '/lib/explorer/types/';
 
 
-//import {toStr} from '@enonic/js-utils';
-//@ts-ignore
+import {toStr} from '@enonic/js-utils/value/toStr';
+
+// Docs: https://developer.enonic.com/docs/http-client-library
+// @ts-expect-error No types yet.
 import {request as httpClientRequest} from '/lib/http-client';
-//@ts-ignore
 import {sleep} from '/lib/xp/task';
 
 
@@ -13,40 +14,54 @@ interface StateRef {
 }
 
 
-//@ts-ignore
+//@ts-ignore TODO Add global types in tsconfig.json
 const {currentTimeMillis} = Java.type('java.lang.System');
 
 
 type SmartRequestParams = HttpClient.Request & {
-	delay? :number
-	retries? :number
-	retryCount? :number
-	stateRef? :StateRef
+	delay?: number
+	retries?: number
+	retryCount?: number
+	stateRef?: StateRef
+	logRequests?: boolean
+	logResponses?: boolean
 };
 
-export function smartRequest({
-	auth,
-	body,
-	connectionTimeout = 20000,
-	contentType,
-	delay = 1000,
-	headers = {},
-	method = 'GET',
-	multipart,
-	params = {},
-	proxy,
-	readTimeout = 5000,
-	retries = 0,
-	retryCount = 0, // Gets incremented on recursion
-	stateRef = { prevReqFinishedAtMillis: null },
-	url
-} :SmartRequestParams) {
+export function smartRequest(smartRequestParams: SmartRequestParams) {
+	const {
+		// Required
+		url,
+		// Optional
+		auth,
+		body,
+		connectionTimeout = 20000, // Default in httpClientRequest is 10000.
+		contentType,
+		delay = 1000,
+		disableHttp2, // Default in httpClientRequest is false.
+		followRedirects,
+		headers, // = {}, // Headers are optional, so no need to default to empty object.
+		method, // = 'GET', // Default already GET in httpClientRequest.
+		multipart,
+
+		// NOTE: Do NOT default to an empty object as that causes contentType
+		// to change into application/x-www-form-urlencoded and browserless
+		// will not work
+		params, // = {},
+
+		proxy,
+		queryParams,
+		readTimeout = 5000, // Default in httpClientRequest is 10000
+		retries = 0,
+		retryCount = 0, // Gets incremented on recursion
+		stateRef = { prevReqFinishedAtMillis: null },
+		// Debugging
+		logRequests = false,
+		logResponses = false,
+	} = smartRequestParams;
+
 	//log.info(toStr({delay, retries, retryCount}));
-	const reqParams :SmartRequestParams = {
+	const reqParams: HttpClient.Request = {
 		connectionTimeout,
-		headers,
-		method,
-		params,
 		readTimeout,
 		url
 	};
@@ -54,11 +69,17 @@ export function smartRequest({
 	if (auth) { reqParams.auth = auth; }
 	if (body) { reqParams.body = body; }
 	if (contentType) { reqParams.contentType = contentType; }
+	if (disableHttp2) { reqParams.disableHttp2 = disableHttp2; }
+	if (followRedirects) { reqParams.followRedirects = followRedirects; }
+	if (headers) { reqParams.headers = headers; }
+	if (method) { reqParams.method = method; }
 	if (multipart) { reqParams.multipart = multipart; }
+	if (params) { reqParams.params = params; }
 	if (proxy) { reqParams.proxy = proxy; }
+	if (queryParams) { reqParams.queryParams = queryParams; }
 	//log.info(toStr({reqParams}));
 
-	let response :HttpClient.Response;
+	let response: HttpClient.Response;
 	try {
 		if (stateRef && stateRef.prevReqFinishedAtMillis && delay) {
 			const msToSleep = delay - (currentTimeMillis() - stateRef.prevReqFinishedAtMillis);
@@ -66,8 +87,10 @@ export function smartRequest({
 			if (msToSleep) { sleep(msToSleep); }
 		}
 		//const sentRequestAtMillis = currentTimeMillis();
+		log[logRequests ? 'info': 'debug']('smartRequest retryCount:%s httpClientRequestParams:%s', retryCount, toStr(reqParams));
 		response = httpClientRequest(reqParams);
 		stateRef.prevReqFinishedAtMillis = currentTimeMillis();
+		log[logResponses ? 'info': 'debug']('smartRequest httpClientRequest Response:%s', toStr(response)); // Sometimes huge!
 		//const latency = stateRef.prevReqFinishedAtMillis - sentRequestAtMillis;
 		//log.info(toStr({latency}));
 	} catch (e) {
@@ -93,13 +116,14 @@ export function smartRequest({
 			log.error(e.message + ': unhandled error on url:' + url, e);
 		}
 		if(retries && retryCount < retries) {
-			reqParams.connectionTimeout = connectionTimeout * 2; // Double on each retry
-			reqParams.delay = delay;
-			reqParams.readTimeout = readTimeout * 2; // Double on each retry
-			reqParams.retries = retries;
-			reqParams.retryCount = retryCount + 1; // Increment before recursion
-			reqParams.stateRef = stateRef; // Pass on reference
-			response = smartRequest(reqParams); // Recursion
+			const retrySmartRequest: SmartRequestParams = JSON.parse(JSON.stringify(reqParams));
+			retrySmartRequest.connectionTimeout = connectionTimeout * 2; // Double on each retry
+			retrySmartRequest.delay = delay;
+			retrySmartRequest.readTimeout = readTimeout * 2; // Double on each retry
+			retrySmartRequest.retries = retries;
+			retrySmartRequest.retryCount = retryCount + 1; // Increment before recursion
+			retrySmartRequest.stateRef = stateRef; // Pass on reference
+			response = smartRequest(retrySmartRequest); // Recursion
 		} else {
 			throw e;
 		}
