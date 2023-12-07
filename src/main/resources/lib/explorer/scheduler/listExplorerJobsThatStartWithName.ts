57f7c05b9b5cd1567cdeb228e5196f828bf9fbdd
Christian Westgaard <comlock@gmail.com>
2023-12-07 13:17:27 +0100

    cleanup

--- src/main/resources/lib/explorer/scheduler/listExplorerJobsThatStartWithName.ts
+++ src/main/resources/lib/explorer/scheduler/listExplorerJobsThatStartWithName.ts
@@ -1,11 +1,11 @@
-// import {toStr} from '@enonic/js-utils';
+// import {toStr} from '@enonic/js-utils/value/toStr';
 import {listExplorerJobs} from '/lib/explorer/scheduler/listExplorerJobs';
 
 
 export function listExplorerJobsThatStartWithName({name}: {name: string}) {
 	const explorerJobs = listExplorerJobs();
-	// log.info(`explorerJobs:${toStr(explorerJobs)}`);
+	// log.info('explorerJobs:%s', toStr(explorerJobs));
 	const explorerJobsThatStartWithName = explorerJobs.filter(({name: n}) => n.startsWith(name));
-	// log.info(`name:${name} explorerJobsThatStartWithName:${toStr(explorerJobsThatStartWithName)}`);
+	// log.info('name:%s explorerJobsThatStartWithName:%s', name, toStr(explorerJobsThatStartWithName));
 	return explorerJobsThatStartWithName;
 }
