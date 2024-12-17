80eb57e5d4cdac8d2022e0deff2d5ee083ebb890
Christian Westgaard <comlock@gmail.com>
2024-12-17 16:13:31 +0100

    Fix #336 Use Request and Response from @enonic-types

--- src/main/resources/lib/explorer/types/Response.d.ts
+++ src/main/resources/lib/explorer/types/Response.d.ts
@@ -1,29 +1,23 @@
-import type {ByteSource} from '@enonic-types/core';
+import type {
+	ByteSource,
+	DefaultResponseHeaders,
+	Response,
+	ResponseHeaders
+} from '@enonic-types/core';
 
-export type PageContributions = {
-	headBegin?: string[]
-	headEnd?: string[]
-	bodyBegin?: string[]
-	bodyEnd?: string[]
-}
+type LowercaseKeys<T extends object> = {
+	[K in keyof T as Lowercase<K>]: T[K];
+};
 
-export type Http2ResponseHeaders = Record<Lowercase<string>, string|number>
+export type Http2ResponseHeaders = LowercaseKeys<ResponseHeaders>
 
-export interface DefaultResponseHeaders extends Http2ResponseHeaders {
-	// HTTP/2 uses lowercase header keys
-	'content-type'?: string
-	'cache-control'?: string
-	etag?: string|number
-}
+export type DefaultHttp2ResponseHeaders = LowercaseKeys<DefaultResponseHeaders>
 
-export type Response<
-	Body = string,
-	Headers extends Http2ResponseHeaders = DefaultResponseHeaders
-> = {
-	body?: Body
-	bodyStream?: ByteSource
-	contentType?: string
-	headers?: Headers
-	pageContributions?: PageContributions
-	status?: number
-}
+export type Http2Response<
+	BODY = string,
+	HEADERS extends Http2ResponseHeaders = DefaultHttp2ResponseHeaders
+> = Response<{
+	body?: BODY
+	bodyStream?: ByteSource // Uncertain if this is correct, maybe only http client responses have this
+	headers?: HEADERS
+}>
